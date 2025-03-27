9d88421aec17f3b9ceaabcf5fd23aaa9c5e184e8
jannik.semperowitsch <jannik.semperowitsch@uni-konstanz.de>
2025-03-27 15:01:27 +0100

    MonoPoly letters in Ascii-art added to main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,29 @@
 @main def hello(): Unit =
+  def text(): Unit = {
+    print("  __      __                           ____           _             \n")
+    print(" |  \\    /  |                         |  _ \\         | |            \n")
+    print(" |   \\  /   |   ___    _____    ___   | |_| |  ___   | |   _    _   \n")
+    print(" | |\\ \\/ /| |  / _ \\  |  _  \\  / _ \\  |  __/  / _ \\  | |  \\ \\  / /  \n")
+    print(" | | \\__/ | | | |_| | | | | | | |_| | | |    | |_| | | |_  \\ \\/ /   \n")
+    print(" |_|      |_|  \\___/  |_| |_|  \\___/  |_|     \\___/  \\__|   \\  /    \n")
+    print("                                                            / /     \n")
+    print("                                                           /_/      \n")
+  }
   println("Hello world!")
   println(msg)
+  for (i<-0 until 8) {
+    for (j<-0 until 8){
+      if ((i+j) %2 == 0){
+        print(shaded)
+      } else {
+        print(" ")
+      }
+    }
+    print("\n")
+  }
+  text()
 
-def msg = "I was compiled by Scala 3. :)"
+def msg = "I am a chess board compiled by Scala 3. :)"
+def shaded = "\u2593"
+
+
