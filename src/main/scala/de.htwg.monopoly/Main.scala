3d08f8219ea2bc1061113855c97cecfb374eaa57
jasemper <jannik.semperowitsch@gmail.com>
2025-05-02 01:53:34 +0200

    added todolist in main

--- src/main/scala/de.htwg.monopoly/Main.scala
+++ src/main/scala/de.htwg.monopoly/Main.scala
@@ -4,4 +4,9 @@ object Main {
   @main def startGame(): Unit = {
     devPlay()
   }
+
+  //TODO: Build folder structure for Model, Controller, TUI, and Observable
+  //TODO: Get all Case Classes own files
+  //TODO: link buying and entering field to addmoney and remove money
+  //TODO: Add tests for all classes
 }
