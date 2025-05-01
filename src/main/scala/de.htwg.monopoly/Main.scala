377b4d8bb3c28080f18e42ee61090b6bbe4ab0e7
jasemper <jannik.semperowitsch@gmail.com>
2025-05-02 01:58:28 +0200

    switched to folder structure

--- src/main/scala/de.htwg.monopoly/Main.scala
+++ src/main/scala/de.htwg.monopoly/Main.scala
@@ -5,7 +5,6 @@ object Main {
     devPlay()
   }
 
-  //TODO: Build folder structure for Model, Controller, TUI, and Observable
   //TODO: Get all Case Classes own files
   //TODO: link buying and entering field to addmoney and remove money
   //TODO: Add tests for all classes
