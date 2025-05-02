c30e375f76df5cf00a52a3e99e5a0ca5d16aa495
jasemper <jannik.semperowitsch@gmail.com>
2025-05-03 01:13:31 +0200

    overall coverage 100

--- src/test/scala/de.htwg.monopoly/MonopolyTester.scala
+++ src/test/scala/de.htwg.monopoly/MonopolyTester.scala
@@ -5,8 +5,20 @@ import de.htwg.monopoly
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers._
 import java.io.{ByteArrayOutputStream, PrintStream}
+import java.io.ByteArrayInputStream
 
 class MonopolyTester extends AnyWordSpec {
+
+  "TUIGame" should {
+    "start the game and call devPlay without exceptions" in {
+      noException should be thrownBy {
+        val simulatedInput = new ByteArrayInputStream("exit\n".getBytes)
+        Console.withIn(simulatedInput) {
+          TUIGame.startGame()
+        }
+      }
+    }
+  }
 
   "data" should {
     "hold the data for players" in {
