ca06342ad7a3a25035588b5671c5d0b55327c369
jasemper <jannik.semperowitsch@gmail.com>
2025-05-02 10:10:02 +0200

    added observertest

---
+++ src/test/scala/de.htwg.monopoly/util/ObservableSpec.scala
@@ -0,0 +1,82 @@
+package de.htwg.monopoly
+
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers._
+
+class ObservableSpec extends AnyWordSpec {
+  
+    "Observable" should {
+        "notify its observers when updated" in {
+            class TestObserver extends Observer {
+            var updated: Boolean = false
+            def update: Unit = updated = true
+            }
+            
+            val observable = new Observable
+            val observer1 = new TestObserver
+            val observer2 = new TestObserver
+            
+            observable.add(observer1)
+            observable.add(observer2)
+            
+            observable.notifyObservers
+            
+            observer1.updated should be (true)
+            observer2.updated should be (true)
+        }
+    
+        "not notify removed observers" in {
+            class TestObserver extends Observer {
+            var updated: Boolean = false
+            def update: Unit = updated = true
+            }
+            
+            val observable = new Observable
+            val observer1 = new TestObserver
+            val observer2 = new TestObserver
+            
+            observable.add(observer1)
+            observable.add(observer2)
+            
+            observable.remove(observer1)
+            
+            observable.notifyObservers
+            
+            observer1.updated should be (false)
+            observer2.updated should be (true)
+        }
+    
+        "allow adding and removing observers" in {
+            class TestObserver extends Observer {
+            var updated: Boolean = false
+            def update: Unit = updated = true
+            }
+            
+            val observable = new Observable
+            val observer1 = new TestObserver
+            val observer2 = new TestObserver
+            
+            observable.add(observer1)
+            observable.add(observer2)
+            
+            observable.notifyObservers
+            
+            observer1.updated should be (true)
+            observer2.updated should be (true)
+            
+            observable.remove(observer1)
+            
+            observable.notifyObservers
+            
+            observer1.updated should be (true)
+            observer2.updated should be (true)
+            
+            observable.add(observer1)
+            
+            observable.notifyObservers
+            
+            observer1.updated should be (true)
+            observer2.updated should be (true)
+        }
+    }
+}
