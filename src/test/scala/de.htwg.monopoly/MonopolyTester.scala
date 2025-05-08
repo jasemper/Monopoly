057ddb04fca4587e724afe27f92864aa517307e7
jasemper <jannik.semperowitsch@gmail.com>
2025-05-08 11:41:50 +0200

    changed up Monopolytester

--- src/test/scala/de.htwg.monopoly/MonopolyTester.scala
+++ src/test/scala/de.htwg.monopoly/MonopolyTester.scala
@@ -9,7 +9,7 @@ import java.io.ByteArrayInputStream
 
 class MonopolyTester extends AnyWordSpec {
 
-  "TUIGame" should {
+  "TUIGame (main)" should {
     "start the game and call devPlay without exceptions" in {
       noException should be thrownBy {
         val simulatedInput = new ByteArrayInputStream("exit\n".getBytes)
@@ -20,26 +20,7 @@ class MonopolyTester extends AnyWordSpec {
     }
   }
 
-  "data" should {
-    "hold the data for players" in {
-      InitPlayers contains Player("Green") shouldBe true
-    }
-    "hold the data for streets" in {
-      InitStreets contains Street("Tennessee Avenue", None, 0, 0, "Orange") shouldBe true
-    }
-    "hold the data for trains" in {
-      InitTrains contains Railroad("B&O Railroad", None) shouldBe true
-    }
-    "hold the data for utilities" in {
-      InitUtilities contains Utility("Water Works", None) shouldBe true
-    }
-    "hold the data for events" in {
-      Events contains Event("Go back 3 spaces", MoveSpaces(-3)) shouldBe true
-    }
-    "hold the data for boards" in {
-      Board contains 0 -> "Go" shouldBe true
-    }
-  }/*
+  /*
   "Monopoly" should {
     "play itself without exceptions" in {
       val controller = new Controller()
